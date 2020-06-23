a30023edfd6a89a9ee77e44cb73685c4f64aa2f6
JoRan29 <jokavana29@gmail.com>
2020-06-23 20:55:09 +0200

    API request

--- sandbox.ts
+++ sandbox.ts
@@ -4,6 +4,10 @@ const decoder = new TextDecoder("utf-8");
 const text = await Deno.readFile("readme.txt");
 console.log(decoder.decode(text));
 
+// reading txt files - does not require decoder
+const data = await Deno.readTextFile("readme.txt");
+console.log(data);
+
 // writing file w/ encoder + --allow-write
 const encoder = new TextEncoder();
 const addText = encoder.encode("More random text, yaaay!");
@@ -11,7 +15,12 @@ const addText = encoder.encode("More random text, yaaay!");
 await Deno.writeFile("readme.txt", addText);
 
 // rename files
-await Deno.rename("readme1.txt", "readme.txt");
+// await Deno.rename("readme1.txt", "readme.txt");
 
 // renome files
 // await Deno.remove("readme.txt");
+
+// fetch API + --allow-net
+const res = await fetch("https://swapi.dev/api/people/");
+const people = await res.json();
+console.log(people);
