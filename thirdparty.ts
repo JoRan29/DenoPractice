a24afaff28610cd51598bdc0b9e02c8bc2ce80f0
JoRan29 <jokavana29@gmail.com>
2020-06-25 17:53:06 +0200

    deno abc - api

--- thirdparty.ts
+++ thirdparty.ts
@@ -20,3 +20,20 @@ import { catYou } from "https://deno.land/x/cat_you/mod.ts";
 
 console.log(catYou());
 console.log(catYou("grumpy"));
+
+// abc - API
+import {
+  Application,
+  Context,
+} from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
+
+const app = new Application();
+
+// routes
+app.get("/", async (ctx: Context) => {
+  console.log("request made!");
+  await ctx.file("./public/index.html");
+});
+
+// listen to port
+app.start({ port: 3000 });
